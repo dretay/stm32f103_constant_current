430d1bebe684494ed0cad84e0f501bedd2db93ff
Drew Tayman <dretay@gmail.com>
2018-01-04 20:49:28 -0500

    cleaning up usb code and app init logic

--- application.c
+++ application.c
@@ -5,10 +5,7 @@ View* views[5];
 void setdat() {
 	LOG("POOP!");
 }
-void StartSysUpdateTask(void const * argument) {
-	osEvent event;	
-	T_SYSTEM_UPDATE *update;
-	
+static void configure_usb_serial_commands() {
 	UsbSerialCommand_Command serialcommands[1];
 	serialcommands[0].command = "dac";
 	serialcommands[0].function = &setdat;
@@ -18,16 +15,22 @@ void StartSysUpdateTask(void const * argument) {
 	serialcommand_config.commands = serialcommands;
 
 	UsbSerialCommand.configure(&serialcommand_config, 1);
-
-
+}
+static void configure_dac() {
 	uint8_t mcp4725_config_cnt = 2;
 	MCP4725_CONFIG mcp4725_configs[mcp4725_config_cnt];
 	mcp4725_configs[1].addr = 0xC4;
 	mcp4725_configs[1].p_i2c = &hi2c2;
 	mcp4725_configs[0].addr = 0xC6;
 	mcp4725_configs[0].p_i2c = &hi2c2;
-	
+	MCP4725.configure(mcp4725_configs, mcp4725_config_cnt);	
 
+	//dac 0 = 0.64v
+	//dac 1 = 10v
+//	MCP4725.set_dac(0, 2000);
+//	MCP4725.set_dac(1, 2000);
+}
+static void configure_rotary_encoders() {
 	//todo: maybe use an add fn rather than hard-coding all this nonsense?
 	ROTARY_ENCODER_CONFIG encoder_configs[2];
 	encoder_configs[0].switch_bus = GPIOA;
@@ -45,23 +48,26 @@ void StartSysUpdateTask(void const * argument) {
 	encoder_configs[1].pin_b_bus = GPIOC;
 	encoder_configs[1].pin_b_idx = GPIO_PIN_15;
 	encoder_configs[1].parameter = VOLTAGE;
-
+	ROTARY_ENCODER.configure(encoder_configs, 2);
+}
+static void configure_graphics() {
 	gfxInit();
 	gdispSetContrast(90);
-
 	views[0] = StatusView.init();
 	
-	MCP4725.configure(mcp4725_configs, mcp4725_config_cnt);	
-	ROTARY_ENCODER.configure(encoder_configs, 2);
-
-	//dac 0 = 0.64v
-	//dac 1 = 10v
-	MCP4725.set_dac(0, 2000);
-	MCP4725.set_dac(1, 2000);
-
-	//todo maybe set this by default on the first view?
+	//todo: maybe set this by default on the first view?
 	views[0]->dirty = true;
 	xTaskNotify(guiDrawTaskHandle, 0x01, eSetBits);
+}
+
+
+void StartSysUpdateTask(void const * argument) {
+	osEvent event;	
+	T_SYSTEM_UPDATE *update;
+	
+	configure_usb_serial_commands();
+	configure_dac();
+	configure_rotary_encoders();
 
 	while (1) {
 		event = osMailGet(SYS_UPDATE_MAILBOX_ID, osWaitForever);
@@ -84,10 +90,10 @@ void StartGUIDrawTask(void const * argument) {
 
 	while (1)
 	{
-		xTaskNotifyWait( pdFALSE,    /* Don't clear bits on entry. */
-			ULONG_MAX,        /* Clear all bits on exit. */
-			NULL, /* Stores the notified value. */
-			osWaitForever);
+		// Don't clear bits on entry., Clear all bits on exit., Stores the notified value.
+		//todo: maybe we should notify on the view we want to be rendered?
+		xTaskNotifyWait(pdFALSE, ULONG_MAX, NULL, osWaitForever);
+		
 		if (views[0]->dirty)
 		{
 			views[0]->dirty = false;
--- usbserialcommand.c
+++ usbserialcommand.c
@@ -11,14 +11,83 @@ extern PCD_HandleTypeDef hpcd;
 int VCP_read(void *pBuffer, int size);
 int VCP_write(const void *pBuffer, int size);
 extern char g_VCPInitialized;
+uint32_t BufPtrOut = 0;
+char UartRxBuffer[RX_DATA_SIZE];
+volatile uint32_t BufPtrIn = 0;                          
+char* last, *token;
+
+
+static void clear_buffer(void) {
+	uint8_t i;
+	for (i = 0; i < RX_DATA_SIZE; i++) {
+		UartRxBuffer[i] = ' ';
+	}
+	BufPtrIn = 0;
+	BufPtrOut = 0;
+}
+static void process_command(void) {
+	uint8_t i;
+
+	token = strtok_r(UartRxBuffer, " ", &last);
+	for (i = 0; i < command_cnt; i++) {
+		if (strncmp(token, config->commands[i].command, strlen(config->commands[i].command)) == 0) {
+			config->commands[i].function();	
+		}
+	}
+}
+void StartSerialCmdTask(void const * argument) {	
+	
+	// Don't clear bits on entry., Clear all bits on exit., Stores the notified value.
+	xTaskNotifyWait(pdFALSE, ULONG_MAX, NULL, osWaitForever);
+
+	/* USER CODE BEGIN 5 */
+	char byte;
+	uint32_t buffptr;
+	uint32_t buffsize;
+	uint8_t i;
+
+	while (1) {
+		if (VCP_read(&byte, 1) != 1)
+			continue;
+		VCP_write(&byte, 1);
+		//increment the "next" pointer
+		UartRxBuffer[BufPtrIn++] = byte;
+
+		//wrap the ring buffer if we've overflowed
+		if (BufPtrIn == RX_DATA_SIZE) {
+			BufPtrIn = 0;
+		}
+
+
+		if (BufPtrOut != BufPtrIn) {
+			if (BufPtrOut > BufPtrIn) /* rollback */ {
+				buffsize = RX_DATA_SIZE - BufPtrOut;
+			}
+			else {
+				buffsize = BufPtrIn - BufPtrOut;
+			}
+
+			buffptr = BufPtrOut;
+
+			BufPtrOut += buffsize;
+			if (BufPtrOut == RX_DATA_SIZE) {
+				BufPtrOut = 0;
+			}
+			for (i = 0; i < buffsize; i++) {
+				//user has finished entering their command - let's show them what they've won
+				if (UartRxBuffer[buffptr + i] == '\r') {
+					process_command();		
+					clear_buffer();							
+				}			
+			}
+		}
+		osThreadYield();
+	}	
+}
 
 static void configure(UsbSerialCommand_Config* config_in, uint8_t cnt_in) {
 	config = config_in;
 	command_cnt = cnt_in;
-
-	
-
-    /* USER CODE BEGIN SysInit */
 
 	USBD_Device.pDesc = &VCP_Desc;
 	USBD_Device.dev_state  = USBD_STATE_DEFAULT;
@@ -26,7 +95,6 @@ static void configure(UsbSerialCommand_Config* config_in, uint8_t cnt_in) {
 	hpcd_USB_FS.pData = &USBD_Device;
 	USBD_Device.pData = &hpcd_USB_FS;
 
-	/* USER CODE BEGIN 2 */
 	HAL_PCDEx_PMAConfig(&hpcd_USB_FS, 0x00, PCD_SNG_BUF, 0x18);
 	HAL_PCDEx_PMAConfig(&hpcd_USB_FS, 0x80, PCD_SNG_BUF, 0x58);
 	HAL_PCDEx_PMAConfig(&hpcd_USB_FS, 0x81, PCD_SNG_BUF, 0x100);
@@ -38,25 +106,12 @@ static void configure(UsbSerialCommand_Config* config_in, uint8_t cnt_in) {
 	xTaskNotify(serialCmdTaskHandle, 0x01, eSetBits);
 
 }
-
-void StartSerialCmdTask(void const * argument) {	
-	
-	// Don't clear bits on entry., Clear all bits on exit., Stores the notified value.
-	xTaskNotifyWait(pdFALSE, ULONG_MAX, NULL, osWaitForever);
-
-	/* USER CODE BEGIN 5 */
-	char byte;
-
-	while (1) {
-		if (VCP_read(&byte, 1) != 1)
-			continue;
-		VCP_write("\r\nYou typed ", 12);
-		VCP_write(&byte, 1);
-		VCP_write("\r\n", 2);
-		osThreadYield();
-	}	
+static char* next() {
+	char *nextToken;
+	nextToken = strtok_r(NULL, " ", &last); 
+	return nextToken; 
 }
-
 const struct usbserialcommand UsbSerialCommand = { 
-	.configure = configure	
+	.configure = configure,	
+	.next = next	
 };
